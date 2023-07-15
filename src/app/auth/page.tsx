f63e4590a9239b6455b35fc54734d947bc24f34d
Oleksandr Shtefan <alex.shtefan3333@gmail.com>
2023-07-15 09:47:51 +0200

    Create Auth Page

---
+++ src/app/auth/page.tsx
@@ -0,0 +1,15 @@
+import LogInForm from "@/components-with-logic/AuthForms/LogInForm";
+import RegisterForm from "@/components-with-logic/AuthForms/RegisterForm";
+const AuthPage = () => {
+  return (
+    <div>
+<h1>Register or Login</h1>
+    <div className="flex flex-col justify-around">
+      <LogInForm />
+      <RegisterForm />
+    </div>
+    </div>
+  );
+};
+
+export default AuthPage;
