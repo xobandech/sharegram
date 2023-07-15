54e3fa1f399bc173f6a4ed49cffdc07b33c5a6d2
Oleksandr Shtefan <alex.shtefan3333@gmail.com>
2023-07-15 09:18:56 +0200

    Add NavBar to layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import './globals.css'
 import { Inter } from 'next/font/google'
-
+import NavigationBar from './components/NavigationBar'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
@@ -14,7 +14,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <NavigationBar />
+        {children}
+      </body>
     </html>
   )
 }
