0850aca5f46cf50f5619e9339ed5fdc8d934a8aa
Oleksandr Shtefan <alex.shtefan3333@gmail.com>
2023-07-15 09:17:08 +0200

    Created main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,34 @@
+"use client"
+import { useEffect, useState } from "react";
+import { getPosts } from "./ServerFunctions";
+
+export type Post = {
+  textToShare: string;
+  imageUrl: string;
+  id: number;
+  likes: number;
+  createdAt: Date;
+  comments: String;
+}
+
 export default function Home() {
+  const [posts, setPosts] = useState<Post[]>([])
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const posts = await getPosts() as Post[]
+      setPosts(posts)
+    };
+
+    fetchPosts();
+  }, []);
+
   return (
     <div>
-      <h1>Homepage</h1>
-    </div>    
-  )
+      {posts &&
+        posts.map((post) => {
+          return <h3 key={post.id}>{post.textToShare}</h3>;
+        })}
+    </div>
+  );
+  
 }
