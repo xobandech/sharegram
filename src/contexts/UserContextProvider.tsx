b1650eeb97520380b69835b8bf0b4e655943d88c
Oleksandr Shtefan <alex.shtefan3333@gmail.com>
2023-07-16 13:22:29 +0200

    Create UserContextProvider & types

--- src/contexts/UserContextProvider.tsx
+++ src/contexts/UserContextProvider.tsx
@@ -1,5 +1,38 @@
-import { useContext } from "react"
+"use client"
+import React, { createContext, useState } from "react";
 
-const UserContextProvider = () => {
-    // const [user, setUser] = useContext()
+export type User = {
+    id: number;
+    username: string;
+    posts?: Post[]
 }
+
+export type Post = {
+    id: number;
+    imageUrl: string;
+    textToShare: string;
+    author: User;
+    createdAt: Date;
+    likes: number;
+    comments: string;
+}
+
+interface UserContextType {
+  currentUser: User | null; 
+  setCurrentUser: (user: User) => void; 
+}
+
+export const UserContext = createContext<UserContextType>({
+  currentUser: null,
+  setCurrentUser: () => null,
+});
+  
+export const UserProvider = ({ children }: { children: React.ReactNode }) => {
+  const [currentUser, setCurrentUser] = useState<any>(null);
+  const value: UserContextType = { currentUser, setCurrentUser };
+  return (
+    <UserContext.Provider value={value}>
+      {children}
+    </UserContext.Provider>
+  );
+};
