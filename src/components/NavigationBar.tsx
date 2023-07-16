5a02e5368cf419c06bc7e1c0620e480d642ba1d4
Oleksandr Shtefan <alex.shtefan3333@gmail.com>
2023-07-16 13:20:27 +0200

    Empty commit

--- src/components/NavigationBar.tsx
+++ src/components/NavigationBar.tsx
@@ -2,16 +2,18 @@ import Link from "next/link";
 import AddPostButton from "../components-with-logic/AddPostButton/AddPostButton";
 const NavigationBar = () => {
   return (
-    <div className="flex justify-between">
-      <div>
-        <Link href="/">Sharegram</Link>
+    <>
+      <div className="flex justify-between">
+        <div>
+          <Link href="/">Sharegram</Link>
+        </div>
+        <div className="flex justify-around w-[15rem]">
+          <Link href="/auth">Sign In</Link>
+          <Link href="/auth">Log In</Link>
+        </div>
       </div>
-      <div className="flex justify-around w-[15rem]">
-        <Link href="/auth">Sign In</Link>
-        <Link href="/auth">Log In</Link>  
-        <AddPostButton />
-      </div>
-    </div>
+      <AddPostButton />
+    </>
   );
 };
 
