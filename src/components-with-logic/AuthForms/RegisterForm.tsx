568e09ce32ddf280280726629e7400591901dc06
Oleksandr Shtefan <alex.shtefan3333@gmail.com>
2023-07-16 12:42:43 +0200

    Make handleRegistration client function

--- src/components-with-logic/AuthForms/RegisterForm.tsx
+++ src/components-with-logic/AuthForms/RegisterForm.tsx
@@ -1,7 +1,19 @@
-import { handleRegistration } from "./LoginServerFunctions";
+"use client"
+import { redirect } from "next/navigation";
+import { createUser } from "./LoginServerFunctions";
+const handleRegistration = async (data: FormData) => {
+  const password = data.get("password")?.valueOf().toString() as string;
+  const confirmPassword = data.get("confirmPassword")?.valueOf() as string;
+  const username = data.get("username")?.valueOf() as string;
+  if (password === confirmPassword) {
+    createUser({username, password})
+    console.log("Successfull registration");
+    redirect("/");
+  }
+};
 const RegisterForm = () => {
   return (
-    <form className="flex " action={handleRegistration}>
+    <form className="flex" action={handleRegistration}>
       <header>Registration</header>
       <label htmlFor="username">Username</label>
       <input className="text-black" type="text" name="username" />
@@ -11,7 +23,7 @@ const RegisterForm = () => {
       <input className="text-black" type="text" name="confirmPassword" />
       <button
         type="submit"
-        className="outline outline-2 outline-white bg-[#443]"
+        className="outline outline-2 outline-black bg-[#443]"
       >
         Sign up
       </button>
