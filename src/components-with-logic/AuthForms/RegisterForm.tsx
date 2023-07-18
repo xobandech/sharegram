5391988cea1c975669adecc194049f46631f1b8d
Oleksandr Shtefan <alex.shtefan3333@gmail.com>
2023-07-18 15:55:07 +0200

    Make setCurrentUser onRegister

--- src/components-with-logic/AuthForms/RegisterForm.tsx
+++ src/components-with-logic/AuthForms/RegisterForm.tsx
@@ -1,19 +1,25 @@
 "use client"
 import { redirect } from "next/navigation";
-import { createUser } from "./LoginServerFunctions";
+import { createUser, findUserByUsername } from "./LoginServerFunctions";
+import { User, UserContext } from "@/contexts/UserContextProvider";
+import { useContext } from "react";
 
-const handleRegistration = async (data: FormData) => {
+
+const RegisterForm = () => {
+  const { setCurrentUser } = useContext(UserContext)
+  const handleRegistration = async (data: FormData) => {
   const password = data.get("password")?.valueOf().toString() as string;
   const confirmPassword = data.get("confirmPassword")?.valueOf() as string;
   const username = data.get("username")?.valueOf() as string;
   if (password === confirmPassword) {
     createUser({username, password})
+    findUserByUsername(username).then((user) => {
+      setCurrentUser(user)
+    })
     console.log("Successfull registration");
     redirect("/");
   }
 };
-
-const RegisterForm = () => {
   return (
     <form className="flex" action={handleRegistration}>
       <header>Registration</header>
