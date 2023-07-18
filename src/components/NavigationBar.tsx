f563fdba54f812312327087311f9eddec7551bf2
Oleksandr Shtefan <alex.shtefan3333@gmail.com>
2023-07-18 17:01:40 +0200

    Add some styling to NavBar

--- src/components/NavigationBar.tsx
+++ src/components/NavigationBar.tsx
@@ -3,9 +3,9 @@ import AddPostButton from "../components-with-logic/AddPostButton/AddPostButton"
 const NavigationBar = () => {
   return (
     <>
-      <div className="flex justify-between">
+      <div className="flex bg-[#f1c9e1] text-[#03017D] justify-between h-10 flex-row items-center">
         <div>
-          <Link href="/">Sharegram</Link>
+          <Link className="pl-4" href="/">Sharegram</Link>
         </div>
         <div className="flex justify-around w-[15rem]">
           <Link href="/auth">Sign In</Link>
