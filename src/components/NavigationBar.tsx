a956056e341a7392235bfce820ab3179ea9b16d2
Oleksandr Shtefan <alex.shtefan3333@gmail.com>
2023-07-20 18:31:04 +0200

    Add Logo to NavBar & Unite Login and Sign in Links

--- src/components/NavigationBar.tsx
+++ src/components/NavigationBar.tsx
@@ -1,15 +1,17 @@
+"use client"
 import Link from "next/link";
+import Image from "next/image";
+import Logo from "../images/logo.png"
 import AddPostButton from "../components-with-logic/AddPostButton/AddPostButton";
 const NavigationBar = () => {
   return (
     <>
-      <div className="flex bg-[#f1c9e1] text-[#03017D] justify-between h-10 flex-row items-center">
+      <div className="flex border-b border-black justify-between h-14 flex-row items-center">
         <div>
-          <Link className="pl-4" href="/">Sharegram</Link>
+          <Link className="pl-4 flex flex-row justify-center" href="/"><Image src={Logo} width={150} height={200} alt="dasds" /></Link>
         </div>
         <div className="flex justify-around w-[15rem]">
-          <Link href="/auth">Sign In</Link>
-          <Link href="/auth">Log In</Link>
+          <Link href="/auth">Sign In | Log In</Link>
         </div>
       </div>
       <AddPostButton />
