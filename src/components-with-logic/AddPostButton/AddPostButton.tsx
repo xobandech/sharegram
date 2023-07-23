9ade797b9473d7e236b6f84bfbb9ea9955be0e21
Oleksandr Shtefan <alex.shtefan3333@gmail.com>
2023-07-23 13:39:06 +0200

    Add some styling to input

--- src/components-with-logic/AddPostButton/AddPostButton.tsx
+++ src/components-with-logic/AddPostButton/AddPostButton.tsx
@@ -1,78 +1,98 @@
 "use client";
-import { FormEvent, useContext, useState } from "react";
+import { FormEvent, useContext, useState, useRef } from "react";
 import { createPost } from "./ServerFunctions";
 import { UserContext } from "@/contexts/UserContextProvider";
 
 const AddPostButton = () => {
-  const [isPopupOpen, setIsPopupOpen] = useState(false);
+  const buttonStyle = {
+    color: "white",
+    backgroundColor: "#1F2937",
+    borderRadius: "0.375rem",
+    fontSize: "0.875rem",
+    padding: "0.625rem",  
+    marginRight: "0.5rem",
+    marginBottom: "0.5rem",
+    fontFamily: "inherit",
+    width: "40%",
+    marginTop: "0.625rem",
+  };
+
   const [formData, setFormData] = useState({
     postText: "",
     postImageUrl: "",
   });
-  const { currentUser } = useContext(UserContext)
+  const { currentUser } = useContext(UserContext);
+  const dialogRef = useRef<HTMLDialogElement>(null);
 
   const handleSubmit = (event: FormEvent) => {
     event.preventDefault();
     createPost(formData.postImageUrl, formData.postText, currentUser);
-    setIsPopupOpen(false);
     setFormData({
       postText: "",
       postImageUrl: "",
     });
+    dialogRef.current?.close();
   };
 
   const openPopup = () => {
-    setIsPopupOpen(true);
+    dialogRef.current?.showModal();
   };
 
   const closePopup = () => {
-    setIsPopupOpen(false);
+    dialogRef.current?.close();
   };
-  if(!currentUser) return <p>Login to Add post</p>
+
   return (
     <>
       <button onClick={openPopup}>Add Post</button>
-      { isPopupOpen && (
-        <div className="popup">
-          <form onSubmit={handleSubmit}>
-            <label htmlFor="imageUrl">
-              Put an image to your post (optional)
-            </label>
-            <input
-              type="text"
-              name="imageUrl"
-              value={formData.postImageUrl}
-              onChange={(e) => {
-                setFormData({
-                  ...formData,
-                  postImageUrl: e.target.value,
-                });
-                console.log(formData);
-                
-              }}
-            />
-            <label htmlFor="textToShare">
-              Put some text
-            </label>
-            <input
-              required
-              type="text"
-              name="textToShare"
-              value={formData.postText}
-              onChange={(e) => {
-                setFormData({
-                  ...formData,
-                  postText: e.target.value,
-                });
-                console.log(formData);
-                
-              }}
-            />
-            <button type="submit">Share!</button>
-            <button onClick={closePopup}>Cancel</button>
-          </form>
-        </div>
-      )}
+      <dialog ref={dialogRef}>
+        <form onSubmit={handleSubmit} className="flex flex-col">
+          <label htmlFor="imageUrl">Put an image to your post (optional)</label>
+          <input
+            style={{ border: "solid", borderWidth: "1px" }}
+            type="text"
+            name="imageUrl"
+            value={formData.postImageUrl}
+            onChange={(e) => {
+              setFormData({
+                ...formData,
+                postImageUrl: e.target.value,
+              });
+            }}
+          />
+          <label htmlFor="textToShare">Put some text</label>
+          <input
+            style={{ border: "solid", borderWidth: "1px" }}
+            required
+            type="text"
+            name="textToShare"
+            value={formData.postText}
+            onChange={(e) => {
+              setFormData({
+                ...formData,
+                postText: e.target.value,
+              });
+            }}
+          />
+          <div className="flex justify-center">
+            <button type="submit" style={buttonStyle}>
+              Post
+            </button>
+          </div>
+        </form>
+        <button
+          className="text-xl p-10 rounded-2xl"
+          onClick={closePopup}
+          style={{
+            border: "solid",
+            borderWidth: "2px",
+            borderRadius: "5px",
+            borderColor: "black",
+          }}
+        >
+          Cancel
+        </button>
+      </dialog>
     </>
   );
 };
