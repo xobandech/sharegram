1dd73d150f59f78eaaea3efd63952af4310b054b
Oleksandr Shtefan <alex.shtefan3333@gmail.com>
2023-07-24 10:11:14 +0200

    Make NavBar <nav> element & make it resposnive

--- src/components/NavigationBar.tsx
+++ src/components/NavigationBar.tsx
@@ -2,20 +2,20 @@
 import Link from "next/link";
 import Image from "next/image";
 import Logo from "../images/logo.png"
+import { UserContext } from "@/contexts/UserContextProvider";
 import AddPostButton from "../components-with-logic/AddPostButton/AddPostButton";
+import { useContext } from "react";
 const NavigationBar = () => {
+  const { currentUser } = useContext(UserContext)
   return (
-    <>
-      <div className="flex border-b border-black justify-between h-14 flex-row items-center">
+    <nav className="sticky z-20 top-0 justify-between [&>.link]:max-xl:hidden w-[100%] p-0 [&>*]:my-auto h-[2.8rem] flex flex-row bg-white border-b">
         <div>
-          <Link className="pl-4 flex flex-row justify-center" href="/"><Image src={Logo} width={150} height={200} alt="dasds" /></Link>
+          <Link className="flex pl-4 flex-row justify-center" href="/"><Image src={Logo} width={150} height={200} alt="dasds" /></Link>
         </div>
-        <div className="flex justify-around w-[15rem]">
-          <Link href="/auth">Sign In | Log In</Link>
+        <div className="text-black pr-5">
+          {currentUser ? <AddPostButton /> : <Link href="/auth">Sign In | Log In</Link>}          
         </div>
-      </div>
-      <AddPostButton />
-    </>
+      </nav>
   );
 };
 
