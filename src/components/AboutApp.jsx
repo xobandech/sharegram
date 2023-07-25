21b2e78fc2943ff59991400f55daad8424c769ad
Oleksandr Shtefan <alex.shtefan3333@gmail.com>
2023-07-25 21:12:58 +0200

    Create AboutApp component which is text about app, planning to add image

---
+++ src/components/AboutApp.jsx
@@ -0,0 +1,11 @@
+const AboutApp = () => {
+    return (
+        <div className="w-[50%] sm:w-[25%] sm:mr-4 pt-6">
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolores rem
+        voluptatibus, nam impedit cumque fugiat dignissimos? Officia
+        voluptatibus voluptates accusamus!
+      </div>
+    )
+}
+
+export default AboutApp
