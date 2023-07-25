3acfb0ff6b6350fa042b78f1c6ddf329a7f378af
Oleksandr Shtefan <alex.shtefan3333@gmail.com>
2023-07-25 21:11:22 +0200

    Restyle Log In form

--- src/components-with-logic/AuthForms/LogInForm.tsx
+++ src/components-with-logic/AuthForms/LogInForm.tsx
@@ -24,19 +24,33 @@ const LogInForm = () => {
   };
 
   return (
-    <form
-      action={handleLogin}
-      className="flex flex-col"
-
-    >
+    <form action={handleLogin} className="flex flex-col ">
       <header className="flex flex-row justify-center">Log In</header>
       <div className="mb-4">
-        <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white" htmlFor="username">Username</label>
-        <input className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" type="text" name="username" />
+        <label
+          className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+          htmlFor="username"
+        >
+          Username
+        </label>
+        <input
+          className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+          type="text"
+          name="username"
+        />
       </div>
       <div className="mb-4">
-        <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white" htmlFor="password">Password</label>
-        <input className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" type="text" name="password" />
+        <label
+          className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+          htmlFor="password"
+        >
+          Password
+        </label>
+        <input
+          className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+          type="text"
+          name="password"
+        />
       </div>
       <button
         type="submit"
@@ -53,6 +67,7 @@ const LogInForm = () => {
           width: "100%",
           maxWidth: "150px", // Set a maximum width for larger screens (sm:w-auto)
           textAlign: "center",
+          marginBottom: "2.5rem",
         }}
       >
         Log In
