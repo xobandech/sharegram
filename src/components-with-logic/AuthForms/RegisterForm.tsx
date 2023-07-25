62bdc6ba219b3c79cc1fd367187e35385350309c
Oleksandr Shtefan <alex.shtefan3333@gmail.com>
2023-07-25 20:57:12 +0200

    Add styling to RegisterForm

--- src/components-with-logic/AuthForms/RegisterForm.tsx
+++ src/components-with-logic/AuthForms/RegisterForm.tsx
@@ -1,37 +1,58 @@
-"use client"
+"use client";
 import { redirect } from "next/navigation";
-import { createUser, findUserByUsername } from "./LoginServerFunctions";
+import { createUser } from "./AuthorizationServerFunctions";
+import { findUserByUsername } from "../AddPostButton/ServerFunctions";
 import { UserContext } from "@/contexts/UserContextProvider";
 import { useContext } from "react";
 
-
 const RegisterForm = () => {
-  const { setCurrentUser } = useContext(UserContext)
+  const { setCurrentUser } = useContext(UserContext);
   const handleRegistration = async (data: FormData) => {
-  const password = data.get("password")?.valueOf().toString() as string;
-  const confirmPassword = data.get("confirmPassword")?.valueOf() as string;
-  const username = data.get("username")?.valueOf() as string;
-  if (password === confirmPassword) {
-    createUser({username, password})
-    findUserByUsername(username).then((user) => {
-      setCurrentUser(user)
-    })
-    console.log("Successfull registration");
-    redirect("/");
-  }
-};
+    const password = data.get("password")?.valueOf().toString() as string;
+    const confirmPassword = data.get("confirmPassword")?.valueOf() as string;
+    const username = data.get("username")?.valueOf() as string;
+    if (password === confirmPassword) {
+      createUser({ username, password });
+      findUserByUsername(username).then((user) => {
+        setCurrentUser(user);
+      });
+      console.log("Successfull registration");
+      redirect("/");
+    }
+  };
   return (
-    <form className="flex" action={handleRegistration}>
-      <header>Registration</header>
-      <label htmlFor="username">Username</label>
-      <input className="text-black" type="text" name="username" />
-      <label htmlFor="password">Password</label>
-      <input className="text-black" type="text" name="password" />
-      <label htmlFor="confirmPassword">Confirm Password</label>
-      <input className="text-black" type="text" name="confirmPassword" />
+    <form
+      action={handleRegistration}
+    >
+      <header className="flex flex-row justify-center">Sign Up</header>
+      <div className="mb-4">
+        <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white" htmlFor="username">Username</label>
+        <input className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" type="text" name="username" />
+      </div>
+      <div className="mb-4">
+        <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white" htmlFor="password">Password</label>
+        <input className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" type="text" name="password" />
+      </div>
+      <div className="mb-4">
+        <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white" htmlFor="confirmPassword">Confirm Password</label>
+        <input className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" type="text" name="confirmPassword" />
+      </div>
       <button
         type="submit"
-        className="outline outline-2 outline-black bg-[#443]"
+        style={{
+          backgroundColor: "#1D4ED8",
+          color: "white",
+          borderColor: "#1D4ED8",
+          borderWidth: "2px",
+          fontSize: "14px",
+          borderRadius: "0.5rem",
+          marginTop: "10px",
+          fontWeight: "500",
+          padding: "10px 20px",
+          width: "100%",
+          maxWidth: "150px", // Set a maximum width for larger screens (sm:w-auto)
+          textAlign: "center",
+        }}
       >
         Sign up
       </button>
@@ -39,4 +60,4 @@ const RegisterForm = () => {
   );
 };
 
-export default RegisterForm
+export default RegisterForm;
