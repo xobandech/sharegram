a6eb59dd1239a25c6b34c4b3ce387b98edd68260
Oleksandr Shtefan <alex.shtefan3333@gmail.com>
2023-07-25 21:26:50 +0200

    Add margin bottom for small screens for text be easy to read

--- src/components/AboutApp.jsx
+++ src/components/AboutApp.jsx
@@ -1,6 +1,6 @@
 const AboutApp = () => {
     return (
-        <div className="w-[50%] sm:w-[25%] sm:mr-4 pt-6">
+        <div className="w-[50%] sm:w-[25%] sm:mr-4 pt-6 max-sm:mb-32">
         Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolores rem
         voluptatibus, nam impedit cumque fugiat dignissimos? Officia
         voluptatibus voluptates accusamus!
