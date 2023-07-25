ef08fd3217c40c999836e91bae6b6d5df1c39ae6
Oleksandr Shtefan <alex.shtefan3333@gmail.com>
2023-07-25 21:12:10 +0200

    Add responsive media queries for AuthPage

--- src/app/auth/page.tsx
+++ src/app/auth/page.tsx
@@ -1,12 +1,14 @@
-import LogInForm from "@/components-with-logic/AuthForms/LogInForm";
-import RegisterForm from "@/components-with-logic/AuthForms/RegisterForm";
+import AuthForm from "@/components-with-logic/AuthForms/AuthForm";
+import AboutApp from "../../components/AboutApp";
 const AuthPage = () => {
   return (
-    <div>
-      <h1>Register or Login</h1>
-      <div className="flex flex-col justify-around">
-        <LogInForm />
-        <RegisterForm />
+    <div className="flex flex-col items-center mt-10 sm:flex-row sm:justify-center">
+      <div className="hidden max-sm:block">
+        <AuthForm />
+      </div>
+      <AboutApp />
+      <div className="max-sm:hidden">
+        <AuthForm />
       </div>
     </div>
   );
