fc933609b22213aef09b0683904b97cfd548142b
Oleksandr Shtefan <alex.shtefan3333@gmail.com>
2023-07-25 20:57:00 +0200

    Add styling to LogInForm

--- src/components-with-logic/AuthForms/LogInForm.tsx
+++ src/components-with-logic/AuthForms/LogInForm.tsx
@@ -1,42 +1,61 @@
-"use client"  
+"use client";
 import { redirect } from "next/navigation";
-import { findUserByUsername } from "./LoginServerFunctions";
+import { findUserByUsername } from "../AddPostButton/ServerFunctions";
 import { useContext } from "react";
 import { UserContext } from "@/contexts/UserContextProvider";
-
 
 const LogInForm = () => {
   const { setCurrentUser } = useContext(UserContext);
   const handleLogin = async (data: FormData) => {
     const password = data.get("password")?.valueOf() as string;
     const username = data.get("username")?.valueOf() as string;
-  
+
     try {
-      findUserByUsername(username)
-        .then((user) => {
-          if (user && password === user.password) {
-            setCurrentUser(user);
-            console.log(user);
-            redirect('/');
-          }
-        });
+      findUserByUsername(username).then((user) => {
+        if (user && password === user.password) {
+          setCurrentUser(user);
+          console.log(user);
+          redirect("/");
+        }
+      });
     } catch (e) {
       console.log(e);
     }
   };
-  
+
   return (
-    <form action={handleLogin} className="outline outline-1 outline-black">
-      <header>Login</header>
-      <label htmlFor="username">Username</label>
-      <input className="text-black" type="text" name="username" />
-      <label htmlFor="password">Password</label>
-      <input className="text-black" type="text" name="password" />
+    <form
+      action={handleLogin}
+      className="flex flex-col"
+
+    >
+      <header className="flex flex-row justify-center">Log In</header>
+      <div className="mb-4">
+        <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white" htmlFor="username">Username</label>
+        <input className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" type="text" name="username" />
+      </div>
+      <div className="mb-4">
+        <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white" htmlFor="password">Password</label>
+        <input className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" type="text" name="password" />
+      </div>
       <button
         type="submit"
-        className="outline outline-2 outline-white bg-[#443]"
+        style={{
+          backgroundColor: "#1D4ED8",
+          color: "white",
+          borderColor: "#1D4ED8",
+          borderWidth: "2px",
+          fontSize: "14px",
+          borderRadius: "0.5rem",
+          marginTop: "10px",
+          fontWeight: "500",
+          padding: "10px 20px",
+          width: "100%",
+          maxWidth: "150px", // Set a maximum width for larger screens (sm:w-auto)
+          textAlign: "center",
+        }}
       >
-        Log in
+        Log In
       </button>
     </form>
   );
