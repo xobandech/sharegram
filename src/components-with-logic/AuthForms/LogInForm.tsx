d7f5f913def7060f3a9757e7d04f199b221ffb4d
Oleksandr Shtefan <alex.shtefan3333@gmail.com>
2023-07-27 13:09:54 +0200

    Add responsivness to Log In form

--- src/components-with-logic/AuthForms/LogInForm.tsx
+++ src/components-with-logic/AuthForms/LogInForm.tsx
@@ -24,7 +24,7 @@ const LogInForm = () => {
   };
 
   return (
-    <form action={handleLogin} className="flex flex-col h-[400px] mb-12 sm:mr-4 ">
+    <form action={handleLogin} className="flex flex-col sm:mr-4 ">
       <header className="flex flex-row justify-center">Log In</header>
       <div className="mb-4">
         <label
@@ -34,7 +34,7 @@ const LogInForm = () => {
           Username
         </label>
         <input
-          className="bg-gray-50 max-w-[220px] border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+          className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
           type="text"
           name="username"
         />
@@ -47,33 +47,34 @@ const LogInForm = () => {
           Password
         </label>
         <input
-          className="bg-gray-50 max-w-[220px] border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+          className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
           type="text"
           name="password"
         />
       </div>
-      <div className="">
-        <button
-          type="submit"
-          style={{
-            backgroundColor: "#1D4ED8",
-            color: "white",
-            borderColor: "#1D4ED8",
-            borderWidth: "2px",
-            fontSize: "14px",
-            borderRadius: "0.5rem",
-            marginTop: "10px",
-            fontWeight: "500",
-            padding: "10px 20px",
-            width: "100%",
-            maxHeight: "44px",
-            maxWidth: "150px", 
-            textAlign: "center",
-          }}
+      <div>
+
+      <button
+        type="submit"
+        style={{
+          backgroundColor: "#1D4ED8",
+          color: "white",
+          borderColor: "#1D4ED8",
+          borderWidth: "2px",
+          fontSize: "14px",
+          borderRadius: "0.5rem",
+          marginTop: "10px",
+          fontWeight: "500",
+          padding: "10px 20px",
+          width: "100%",
+          maxWidth: "150px", // Set a maximum width for larger screens (sm:w-auto)
+          textAlign: "center",
+          marginBottom: "2.5rem",
+        }}
         >
-          Log In
-        </button>
-      </div>
+        Log In
+      </button>
+        </div>
     </form>
   );
 };
