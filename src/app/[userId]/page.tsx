b44534090d6f8eafd260b7a2c5c3b812d2f4b0b8
Oleksandr Shtefan <alex.shtefan3333@gmail.com>
2023-07-29 19:31:39 +0200

    Add rendering userPosts&information about user

--- src/app/[userId]/page.tsx
+++ src/app/[userId]/page.tsx
@@ -1,18 +1,46 @@
 "use client"
-import { findUserById } from "@/components-with-logic/AddPostButton/ServerFunctions"
+import { findUserById, getPostsByUserId } from "@/components-with-logic/AddPostButton/ServerFunctions"
 import { User } from "@/contexts/UserContextProvider"
+import { UserPost } from "@prisma/client"
 import { useParams } from "next/navigation"
-import { useState } from "react"
+import { useEffect, useState } from "react"
 
 const UserProfilePage = () => {
-    const [user, setUser] = useState<User>()
-    const userId = +useParams().userId
-    findUserById(userId).then((user) => setUser(user))
-    console.log(user);
-    
-    return <>
+  const [user, setUser] = useState<User>()
+  const [userPosts, setUserPosts] = useState<UserPost[]>([]);
+  const userId = +useParams().userId
 
+  useEffect(() => {
+    findUserById(userId).then((user) => setUser(user));
+
+    getPostsByUserId(userId).then((posts) => setUserPosts(posts));
+  }, [userId]);
+
+  return (
+    <>
+      <div>
+        {user && (
+          <>
+            <h2>User ID: {user.id}</h2>
+            <h2>Username: {user.username}</h2>
+          </>
+        )}
+
+        <div>
+          {userPosts.length > 0 ? (
+            userPosts.map((post) => (
+              <div key={post.id}>
+                <h3>Post ID: {post.id}</h3>
+                <p>Post Text: {post.postText}</p>
+              </div>
+            ))
+          ) : (
+            <p>No posts found for this user.</p>
+          )}
+        </div>
+      </div>
     </>
+  );
 }
 
-export default UserProfilePage
+export default UserProfilePage;
