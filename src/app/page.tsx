0fe27a464c4619ea72eda8d750d53934c5de915c
Oleksandr Shtefan <alex.shtefan3333@gmail.com>
2023-07-30 13:10:51 +0200

    Make post rendering with UserPostComponent

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,14 +2,12 @@
 import { useEffect, useState } from "react";
 import { getPosts } from "../ServerFunctions";
 import { UserPost } from "@prisma/client";
-import Link from "next/link";
-import Image from "next/image";
+import UserPostComponent from "@/components/Post";
 export default function Home() {
   const [posts, setPosts] = useState<UserPost[]>([]);
   useEffect(() => {
     const fetchPosts = async () => {
       await getPosts().then((posts) => {
-        console.log(posts);
         setPosts(posts as UserPost[]);
       });
     };
@@ -21,57 +19,7 @@ export default function Home() {
     <div className="flex flex-col-reverse items-center">
       {posts &&
         posts.map((post) => {
-          if (!post.postImageUrl)
-            return (
-              <div
-                key={post.id}
-                className="flex w-[468px] max-sm:w-[100%] flex-col border-b"
-              >
-                <Link
-                  className="flex flex-row w-[100%] items-left m-2"
-                  href={`/${post.userId}`}
-                >
-                  <Image
-                    className="rounded-2xl"
-                    src={`https://robohash.org/${post.userId}?size=40x40&set=set2`}
-                    alt={`${post.authorUsername}'s avatar`}
-                    width={40}
-                    height={40}
-                  />
-                  <p className="p-2">{post.authorUsername}</p>
-                </Link>
-                <h3>{post.postText}</h3>
-              </div>
-            );
-
-          return (
-            <div
-              key={post.id}
-              className="flex w-[468px] max-sm:w-[100%] flex-col border-b"
-            >
-              <Link
-                className="flex flex-row w-[100%] items-left m-2"
-                href={`/${post.userId}`}
-              >
-                <Image
-                  className="rounded-2xl"
-                  src={`https://robohash.org/${post.userId}?size=40x40&set=set2`}
-                  alt={`${post.authorUsername}'s avatar`}
-                  width={40}
-                  height={40}
-                />
-                <p className="p-2">{post.authorUsername}</p>
-              </Link>
-              <div className="max-w-[468px] max-sm:max-w-[100%] bg-gray-50 overflow-hidden">
-                <img
-                  src={post.postImageUrl}
-                  alt={post.postImageUrl}
-                  className="object-contain w-full h-full max-sm:w-[100%] max max-w-[468px] max-h-[585px]"
-                />
-              </div>
-              <h3 className="text-xl">{post.postText}</h3>
-            </div>
-          );
+          return <UserPostComponent {...post} />
         })}
     </div>
   );
