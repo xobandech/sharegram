bf5581f2e440f0ce796936d5ea2b5360665866c7
Oleksandr Shtefan <alex.shtefan3333@gmail.com>
2023-07-31 22:22:30 +0200

    Make users return without password field

--- src/components-with-logic/AuthForms/AuthorizationServerFunctions.ts
+++ src/components-with-logic/AuthForms/AuthorizationServerFunctions.ts
@@ -26,5 +26,8 @@ export async function findUsersBySearchQuery(searchQuery: string) {
 
 export async function getUsers() {
   "use server";
-  return prisma.user.findMany();
+  return prisma.user.findMany({ select: {
+    id: true,
+    username: true,
+  }});
 }
