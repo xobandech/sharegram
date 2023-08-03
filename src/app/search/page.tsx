9df44aefe7e573849af4191206ddfb2651eaeefc
Oleksandr Shtefan <alex.shtefan3333@gmail.com>
2023-08-03 18:35:06 +0200

    Add conditional rendering for phone & pc

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -1,12 +1,17 @@
-"use client"
-import SearchUserComponent from "@/components-with-logic/SearchUserComponent/SearchUserComponent"
+"use client";
+import SearchUserComponent from "@/components-with-logic/SearchUserComponent/SearchUserComponent";
 
 const SearchPage = () => {
-    return (
-        <div className="flex">
-            <SearchUserComponent top={77} />
-        </div>
-    )
-}
+  return (
+    <div className="flex justify-center mt-2">
+      <div className="sm:hidden">
+        <SearchUserComponent top={85} />
+      </div>
+      <h1 className="max-sm:hidden">
+        This page is for mobile version of site.
+      </h1>
+    </div>
+  );
+};
 
-export default SearchPage
+export default SearchPage;
