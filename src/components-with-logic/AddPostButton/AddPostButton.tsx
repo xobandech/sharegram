800594e0ecd395779b56a1b796cf6821c00bc81a
Oleksandr Shtefan <alex.shtefan3333@gmail.com>
2023-08-03 18:36:49 +0200

    Add image loading from user storage

--- src/components-with-logic/AddPostButton/AddPostButton.tsx
+++ src/components-with-logic/AddPostButton/AddPostButton.tsx
@@ -1,35 +1,47 @@
 "use client";
-import { FormEvent, useContext, useState, useRef } from "react";
+import { FormEvent, useContext, useState, useRef, ChangeEvent } from "react";
 import { createPost } from "./ServerFunctions";
 import { UserContext } from "@/contexts/UserContextProvider";
+const buttonStyle = {
+  color: "white",
+  backgroundColor: "#1F2937",
+  borderRadius: "0.375rem",
+  fontSize: "0.875rem",
+  padding: "0.625rem",  
+  marginRight: "0.5rem",
+  marginBottom: "0.5rem",
+  fontFamily: "inherit",
+  width: "40%",
+  marginTop: "0.625rem",
+};
 
 const AddPostButton = () => {
-  const buttonStyle = {
-    color: "white",
-    backgroundColor: "#1F2937",
-    borderRadius: "0.375rem",
-    fontSize: "0.875rem",
-    padding: "0.625rem",  
-    marginRight: "0.5rem",
-    marginBottom: "0.5rem",
-    fontFamily: "inherit",
-    width: "40%",
-    marginTop: "0.625rem",
-  };
-
+  const [base64Image, setBase64Image] = useState('');
   const [formData, setFormData] = useState({
     postText: "",
-    postImageUrl: "",
   });
+
+  const handleImageChange = (event: ChangeEvent<HTMLInputElement>) => {
+    const file = event.target?.files?.[0]
+    const reader = new FileReader();
+    reader.onloadend = () => {
+      const base64String = reader.result;
+      setBase64Image(base64String as string);
+    };
+
+    if (file) {
+      reader.readAsDataURL(file);
+    }
+  };
+
   const { currentUser } = useContext(UserContext);
   const dialogRef = useRef<HTMLDialogElement>(null);
 
   const handleSubmit = (event: FormEvent) => {
     event.preventDefault();
-    createPost(formData.postImageUrl, formData.postText, currentUser);
+    createPost(base64Image, formData.postText, currentUser);
     setFormData({
       postText: "",
-      postImageUrl: "",
     });
     dialogRef.current?.close();
   };
@@ -48,18 +60,7 @@ const AddPostButton = () => {
       <dialog ref={dialogRef}>
         <form onSubmit={handleSubmit} className="flex flex-col">
           <label htmlFor="imageUrl">Put an image to your post (optional)</label>
-          <input
-            style={{ border: "solid", borderWidth: "1px" }}
-            type="text"
-            name="imageUrl"
-            value={formData.postImageUrl}
-            onChange={(e) => {
-              setFormData({
-                ...formData,
-                postImageUrl: e.target.value,
-              });
-            }}
-          />
+          <input type="file" accept="image/*" onChange={handleImageChange} placeholder="Upload image"/>
           <label htmlFor="textToShare">Put some text</label>
           <input
             style={{ border: "solid", borderWidth: "1px" }}
