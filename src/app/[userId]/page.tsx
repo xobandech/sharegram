b90fda28b6ad04500a19ce37392aee3e47a23fc1
Oleksandr Shtefan <alex.shtefan3333@gmail.com>
2023-08-04 11:22:37 +0200

    Add webkitborderradius to style

--- src/app/[userId]/page.tsx
+++ src/app/[userId]/page.tsx
@@ -30,7 +30,8 @@ const UserProfilePage = () => {
               <Image
                 className="rounded-full mr-2 outline outline-solid outline-black outline-1"
                 style={{
-                  borderRadius: "9999px"
+                  borderRadius: "9999px",
+                  WebkitBorderRadius: "9999px"
                 }}
                 src={`https://robohash.org/${user.id}?size=65x65&set=set2`}
                 alt={`${user.username}'s avatar`}
