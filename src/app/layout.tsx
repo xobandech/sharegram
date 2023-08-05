4b917154ae2f8e30fbb2e09dfba7e5285fb398a8
Oleksandr Shtefan <alex.shtefan3333@gmail.com>
2023-08-05 14:35:32 +0200

    Wrap app in UpdaterProvider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import "../globals.css";
 import { Inter } from "next/font/google";
 import NavigationBar from "../components/NavigationBar";
 import { UserProvider } from "@/contexts/UserContextProvider";
+import { UpdaterProvider } from "@/contexts/UpdaterContextProvider";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
@@ -15,14 +16,18 @@ export default function RootLayout({
 }) {
   return (
     <UserProvider>
-      <html lang="en">
-        <body className={`${inter.className}`}>
-          <NavigationBar />
-          <div className="flex min-h-[100vh]  justify-center bg-[#f4f4f4]">
-            <div className="md:w-[1000px] max-md:w-[100vw] bg-white">{children}</div>
-          </div>
-        </body>
-      </html>
+      <UpdaterProvider>
+        <html lang="en">
+          <body className={`${inter.className}`}>
+            <NavigationBar />
+            <div className="flex min-h-[100vh]  justify-center bg-[#f4f4f4]">
+              <div className="md:w-[1000px] max-md:w-[100vw] bg-white">
+                {children}
+              </div>
+            </div>
+          </body>
+        </html>
+      </UpdaterProvider>
     </UserProvider>
   );
 }
