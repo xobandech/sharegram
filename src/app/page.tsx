4e089463d25501421b2390f581a9128d09b66175
Oleksandr Shtefan <alex.shtefan3333@gmail.com>
2023-08-05 14:36:09 +0200

    Add updaterValue in dependency array of useEffect fetching posts

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,22 @@
 "use client";
-import { useEffect, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import { getPosts } from "../ServerFunctions";
 import { UserPost } from "@prisma/client";
 import UserPostComponent from "@/components/UserPostComponent";
+import { UpdaterContext } from "@/contexts/UpdaterContextProvider";
 export default function Home() {
+  const { updaterValue, setUpdaterValue } = useContext(UpdaterContext)
   const [posts, setPosts] = useState<UserPost[]>([]);
   useEffect(() => {
     const fetchPosts = async () => {
       await getPosts().then((posts) => {
         setPosts(posts as UserPost[]);
       });
+      setUpdaterValue(false)
     };
 
     fetchPosts();
-  }, []);
+  }, [updaterValue]);
 
   return (
     <div className="flex flex-col-reverse items-center">
